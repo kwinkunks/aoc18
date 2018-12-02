e2ff224d240eeceaa9ff14ea8cd210aab7e1b858
kwinkunks <matt@agilegeoscience.com>
2018-12-02 12:36:18 +0000

    day 1 solution

---
+++ day1.py
@@ -0,0 +1,50 @@
+# -*- coding: utf 8 -*-
+"""
+Advent of Code 2018
+Day 1
+"""
+
+
+def part1():
+    """Compute the cumulative sum of the diffs in the input.
+    """
+    freq = 0
+    with open("input/day1.txt", 'r') as f:
+        for d in f:
+            freq += float(d)
+    return freq
+
+
+def part2(iter=0, freq=0, freqs=None):
+    """Keep computing the cumulative sum of the numbers in the input,
+    until we get the same sum twice. Keep count of the file iterations
+    too, just for fun.
+    """
+    freqs = {0} if freqs is None else freqs
+
+    with open("input/day1.txt", 'r') as f:
+        data = f.readlines()
+
+    for d in data:
+        freq += float(d)
+
+        if freq in freqs:
+            break
+        else:
+            freqs.add(freq)
+    
+    else:
+        freq, iter = part2(iter=iter+1, freq=freq, freqs=freqs)
+
+    return freq, iter
+
+
+if __name__ == "__main__":
+    import sys
+    if sys.argv[1] == '1':
+        print(part1())
+    elif sys.argv[1] == '2':
+        freq, iter = part2()
+        print(f"Got {freq} after {iter} iterations.")
+    else:
+        print("Which part?")
