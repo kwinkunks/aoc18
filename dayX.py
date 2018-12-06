d6a29e1f402ff03573b2ffc80d395d4b32a2fe2d
kwinkunks <matt@agilegeoscience.com>
2018-12-06 18:07:55 +0000

    catch up

---
+++ dayX.py
@@ -0,0 +1,27 @@
+# -*- coding: utf 8 -*-
+"""
+Advent of Code 2018
+Day X
+"""
+
+
+def part1():
+    """Part 1.
+    """
+    return
+
+
+def part2():
+    """Part 2.
+    """
+    return
+
+
+if __name__ == "__main__":
+    import sys
+    if sys.argv[1] == '1':
+        print(part1())
+    elif sys.argv[1] == '2':
+        print(part2())
+    else:
+        print("Which part?")
