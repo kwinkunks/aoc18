afee2cdc59f946005af2db77d4643a0d20ef8f15
kwinkunks <matt@agilegeoscience.com>
2018-12-11 12:05:06 -0400

    god-awful so-called solution

--- day8.py
+++ day8.py
@@ -3,6 +3,7 @@
 Advent of Code 2018
 Day 8
 """
+import time
 
 with open('input/day8.txt') as f:
     data = f.read()
@@ -14,7 +15,7 @@ def part1():
     seq = [int(d) for d in data.split()]
 
     meta = []
-    job_stack = []
+    job_stack = [0]
 
     while seq:
         
@@ -42,9 +43,68 @@ def part1():
 
 def part2():
     """Part 2.
+
+    Not proud of this... it's brutal. Especially considering how terse
+    Part 1 is. Pfff...
     """
 
-    return
+    def get_id():
+        # Needs to avoid collisions and be in order.
+        # Such a hack. For shame.
+        return int((1e9*time.time())%1e12)
+
+    seq = [int(d) for d in data.split()]
+    
+    all_meta = []
+    complete = []
+    parent_id = get_id()
+    job_stack = [{'parent': parent_id, 'id': get_id(), 'meta': [], 'layer': 0}]
+    layer = 0
+
+    while seq:
+        job = job_stack.pop()
+        layer_ = job['layer']
+        
+        # Process the job if not empty.
+        if job['meta']:
+            for _ in range(job['meta'].pop()):
+                this = seq.pop(0)
+                job['meta'].append(this)
+                all_meta.append(this)
+            complete.append(job)
+            parent_id = job['parent']
+
+
+        else:
+            c, m = seq.pop(0), seq.pop(0)
+                    
+            this_id = get_id()
+            job_stack.append({'parent': parent_id, 'id': this_id, 'meta': [m], 'score': 0, 'children': c, 'layer': layer_})
+            parent_id = this_id
+            layer += 1
+            layer_ = layer
+
+            for c_ in range(c):
+                
+                job_stack.append({'parent': parent_id, 'id': get_id(), 'meta': [], 'layer': layer_})
+            
+    # Now we can process the result to traverse the tree and get the answer.
+    # This is... awful.
+    for item in complete:
+        if item['children'] == 0:
+            item['score'] = sum(item['meta'])
+        else:
+            children = [i for i in complete if i['parent']==item['id']]
+            children_ = sorted(children, key=lambda d: d['id'])
+            for idx in item['meta']:
+                try:
+                    item['score'] += children_[idx-1]['score']
+                except:
+                    pass
+
+    result, = [i['score'] for i in complete if i['layer'] == 0]
+
+    return result
 
 
 if __name__ == "__main__":
